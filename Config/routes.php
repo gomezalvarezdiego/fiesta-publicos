3172a083f5969de300720862331c6e731109d406
Diego Gomez <holosglobalis@gmail.com>
2016-08-18 23:01:07 -0500

    Open application

--- Config/routes.php
+++ Config/routes.php
@@ -18,8 +18,8 @@
  */
 	Router::parseExtensions('json', 'xml');
 	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-	Router::connect('/', array('controller' => 'users', 'action' => 'login_alterno'));
-	//Router::connect('/', array('controller' => 'users', 'action' => 'login'));
+	//Router::connect('/', array('controller' => 'users', 'action' => 'login_alterno'));
+	Router::connect('/', array('controller' => 'users', 'action' => 'login'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
